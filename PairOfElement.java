8b2b580a000c92ea9350fdbe4c986b8f3fd67705
shaukhk01 <shaukhk01@gmail.com>
2020-03-27 19:44:05 +0530

    sortedArray pairvalue

--- PairOfElement.java
+++ PairOfElement.java
@@ -25,21 +25,39 @@ public class PairOfElement
             {
                 if(Hash[k-arr2[n]]!=0)
             {
-                System.out.print(arr2[n]+" ");
+//                System.out.print(arr2[n]+" ");
             }
     }
     }
     }
-    
+    public static void printSortedArray(int arr3[],int len3)
+    {
+        int c=10;
+        int q=len3-1;
+        for(int x=0;x<len3;x++)
+        {
+            for(int z=q;z>0;z--)
+            {
+                if(arr3[x]+arr3[z]==c)
+                {
+                    System.out.println(arr3[x]+"+"+arr3[z]);
+                      
+                }
+            }
+        }
+    }
     public static void main(String[]args)
     {
         int[] arr={6,3,8,10,16,7,5,2,9,14};
         int len=arr.length;
         printSum(arr,len);
-        int[] arr2={6,3,8,10,16,7,4,2,9,14};
+        int[] arr2={6,3,8,10,16,7,4,2,9,14};//unsorted array
         int[] Hash=new int[16+1];
         int l2=arr2.length;
         int k=10;
         printHash(arr2,Hash,l2,k);
+        int[] arr3={1,3,4,5,6,8,9,10,12,14};//sortedArray
+        int l3 = arr3.length;
+        printSortedArray(arr3,l3);
     }
 }
