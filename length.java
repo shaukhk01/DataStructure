69e0072cf415f19002a66782056c17c81f264b85
shaukhk01 <shaukhk01@gmail.com>
2020-03-28 13:06:15 +0530

    string length without length()method

---
+++ length.java
@@ -0,0 +1,16 @@
+class LengthOfString
+{
+    public static void main(String[]args)
+    {
+       // char[] c={'a','b','c','d','e'};
+       String c="hello";
+        int count=0;
+        for(char a:c.toCharArray())
+        {
+            count++;
+        }
+        System.out.println(count);
+    }
+}
+
+
