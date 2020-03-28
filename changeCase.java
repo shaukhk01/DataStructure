5f6bb30ae34d20269fc958d5dccb26e60a5d9c66
shaukhk01 <shaukhk01@gmail.com>
2020-03-28 12:22:50 +0530

    lower To Upper AND Upper To lower

--- changeCase.java
+++ changeCase.java
@@ -3,7 +3,7 @@ public class changeCase
     public static void main(String[]args)
     {
         //String str="HELLO ANNIE";
-          String str="Hello Annie";
+          String str="hello ANNIE";
         char[] ch=str.toCharArray();//change string into array
         for(int i=0;i<ch.length;i++)
         {
@@ -13,6 +13,8 @@ public class changeCase
                                 *then we can get corresponding result
                                */
             }
+            else if(ch[i]>=97 && ch[i]<=122)
+                ch[i]-=(char)32;
 
             System.out.print(ch[i]);
         }
